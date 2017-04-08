3a9debd545eb6780d2663ada94c5f7896198be4a
ponggit <pongsatt@gmail.com>
2017-04-09 01:45:39 +0700

    Update request version to see if error ENOTFOUND

--- es/client.js
+++ es/client.js
@@ -5,12 +5,9 @@ const search_url = Config.ESTAURANT_API_URL + '/estaurant/_search';
 
 const search = (query) => {
     let opts = {
-        host: Config.ESTAURANT_API_HOST,
-        port: Config.ESTAURANT_API_PORT,
-        path: '/estaurant/_search',
         method: 'POST',
+        url: search_url,
         json: true,
-        timeout: 20000,
         headers : {
             "Authorization" : "Basic " + new Buffer(Config.ESTAURANT_API_AUTH).toString("base64")
         },
