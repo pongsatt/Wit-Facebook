15af7bc3a22e5284d4353db26f1b52d0449b6e63
ponggit <pongsatt@gmail.com>
2017-04-09 02:17:33 +0700

    Use unirest instead of request

--- es/client.js
+++ es/client.js
@@ -1,27 +1,32 @@
 'use strict';
 const request = require('request-promise');
 const Config = require('../config/const');
+var unirest = require('unirest');
 
 const options = {
-  uri: Config.ESTAURANT_API_URL + '/estaurant/_search',
-  method: 'POST',
-  json: true,
-  headers: {
-    'Content-Type': 'application/json',
-    "Authorization" : "Basic " + new Buffer(Config.ESTAURANT_API_AUTH).toString("base64")
-  },
+    uri: Config.ESTAURANT_API_URL + '/estaurant/_search',
+    method: 'POST',
+    json: true,
+    headers: {
+        'Content-Type': 'application/json',
+        "Authorization": "Basic " + new Buffer(Config.ESTAURANT_API_AUTH).toString("base64")
+    },
 };
 
 const search = (query) => {
-    let opts = Object.assign({}, options, {body: query});
+    let opts = Object.assign({}, options, { body: query });
 
-    console.log('Request options: ', opts);
+    let userPass = Config.ESTAURANT_API_AUTH.split(':');
 
-    return request(opts)
-        .then((data) => {
-            // console.log('Found data: ', data);
-            return data;
+    return new Promise((resolve, reject) => {
+        return unirest.post(Config.ESTAURANT_API_URL + '/estaurant/_search')
+        .headers({ 'Content-Type': 'application/json' })
+        .auth({ user: userPass[0], pass: userPass[1]})
+        .send(query)
+        .end(function (response) {
+            return resolve(response.body);
         });
+    });
 }
 
 module.exports = {
