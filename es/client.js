9cb785b5f70b5ce69e4c92ba72ca2b199e663ef2
ponggit <pongsatt@gmail.com>
2017-04-09 03:17:50 +0700

    Change back to local es

--- es/client.js
+++ es/client.js
@@ -3,13 +3,25 @@ const request = require('request-promise');
 const Config = require('../config/const');
 var unirest = require('unirest');
 
+const options = {
+    uri: Config.ESTAURANT_API_URL + '/estaurant/_search',
+    method: 'POST',
+    json: true,
+    headers: {
+        'Content-Type': 'application/json',
+        "Authorization": "Basic " + new Buffer(Config.ESTAURANT_API_AUTH).toString("base64")
+    },
+};
+
 const search = (query) => {
+    let opts = Object.assign({}, options, { body: query });
+
     let userPass = Config.ESTAURANT_API_AUTH.split(':');
 
     return new Promise((resolve, reject) => {
-        return unirest.post('https://site:9ba59ac512ad38d86747aea0ca255596@nori-us-east-1.searchly.com/estaurant/_search')
+        return unirest.post(Config.ESTAURANT_API_URL + '/estaurant/_search')
         .headers({ 'Content-Type': 'application/json' })
-        // .auth({ user: userPass[0], pass: userPass[1]})
+        .auth({ user: userPass[0], pass: userPass[1]})
         .send(query)
         .end(function (response) {
             return resolve(response.body);
