0e7a470d2e69c1c393f7413e5239633c438cc274
ponggit <pongsatt@gmail.com>
2017-04-09 03:09:31 +0700

    Test another website fix

--- es/client.js
+++ es/client.js
@@ -3,25 +3,13 @@ const request = require('request-promise');
 const Config = require('../config/const');
 var unirest = require('unirest');
 
-const options = {
-    uri: 'https://site:9ba59ac512ad38d86747aea0ca255596@nori-us-east-1.searchly.com',
-    method: 'POST',
-    json: true,
-    headers: {
-        'Content-Type': 'application/json',
-        // "Authorization": "Basic " + new Buffer(Config.ESTAURANT_API_AUTH).toString("base64")
-    },
-};
-
 const search = (query) => {
-    let opts = Object.assign({}, options, { body: query });
-
     let userPass = Config.ESTAURANT_API_AUTH.split(':');
 
     return new Promise((resolve, reject) => {
-        return unirest.post(Config.ESTAURANT_API_URL + '/estaurant/_search')
+        return unirest.post('https://site:9ba59ac512ad38d86747aea0ca255596@nori-us-east-1.searchly.com/estaurant/_search')
         .headers({ 'Content-Type': 'application/json' })
-        .auth({ user: userPass[0], pass: userPass[1]})
+        // .auth({ user: userPass[0], pass: userPass[1]})
         .send(query)
         .end(function (response) {
             return resolve(response.body);
